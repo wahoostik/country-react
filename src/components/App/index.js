d6584f1f23da4bbbfb65861294fe2ea323ffbea6
wahoostik <anthonypaccaud@wanadoo.fr>
2021-08-10 15:02:30 +0200

    folder pages

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,12 @@
-import logo from '../../logo.svg';
+import Home from '../../pages/Homes';
 import './styles.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Country in the World</h1>
+        <Home />
       </header>
     </div>
   );
---
+++ src/pages/Homes.js
@@ -0,0 +1,8 @@
+const Home = () => (
+  <div className='home'>
+    <h1>Composant : Home</h1>
+  </div>
+);
+
+// == Export
+export default Home;

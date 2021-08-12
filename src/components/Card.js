d0f7523a7e222109fc89ee1e9c78ef997a3087bf
wahoostik <anthonypaccaud@wanadoo.fr>
2021-08-12 16:54:07 +0200

    styles countries & card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,23 @@
 // == Composant
 const Card = ({country}) => {
     console.log(country);
+
+    // regex qui permet de placer un espace après 3 chiffres
+    const numberFormat = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    };
+    
     return (
-  <div className='card'>
-    <h1>Composant : Card</h1>
-  </div>
+<li className='card'>
+        <img src={country.flag} alt='flag'></img>
+        <div className='data-container'>
+            <ul>
+                <li>{country.name}</li>
+                <li>{country.capital}</li>
+                <li>Population : {numberFormat(country.population)}</li>
+            </ul>
+        </div>
+</li>
 )};
 
 // == Export
